faf6105a57740e5a96cad477c0ba7e19069156c7
Lenita Costa <lenytamarttins@gmail.com>
2024-03-11 01:40:42 -0300

    feat; insert UserEntity

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,29 +1,17 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUser.dto';
-<<<<<<< HEAD
 import { UserService } from './user.service';
-import { User } from './interfaces/user.interface';
+import { UserEntity } from './interfaces/user.entity';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
   @Post()
-  async createUser(@Body() createUser: CreateUserDto): Promise<User> {
+  async createUser(@Body() createUser: CreateUserDto): Promise<UserEntity> {
     return this.userService.createUser(createUser);
   }
   @Get()
-  async getAllUser(): Promise<User[]> {
+  async getAllUser(): Promise<UserEntity[]> {
     return this.userService.getAllUser();
-=======
-
-@Controller('user')
-export class UserController {
-  @Post()
-  async createUser(@Body() createUser: CreateUserDto) {
-    return {
-      ...createUser,
-      password: undefined,
-    };
->>>>>>> 00f95ca4b2258229bf3f04d8a053f913ffb123db
   }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
-<<<<<<< HEAD
 import { UserService } from './user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './interfaces/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
-  providers: [UserService]
-=======
-
-@Module({
-  controllers: [UserController]
->>>>>>> 00f95ca4b2258229bf3f04d8a053f913ffb123db
+  providers: [UserService],
 })
 export class UserModule {}
